59052aad633821045bf48de67e197dfe13c746fc
Yifan Cai <ycai@apache.org>
2024-01-30 18:10:41 -0800

    fix test

--- cassandra-four-zero/src/test/java/org/apache/cassandra/bridge/SSTableWriterImplementationTest.java
+++ cassandra-four-zero/src/test/java/org/apache/cassandra/bridge/SSTableWriterImplementationTest.java
@@ -21,6 +21,7 @@ package org.apache.cassandra.bridge;
 
 import java.io.File;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -46,7 +47,7 @@ class SSTableWriterImplementationTest
     File writeDirectory;
 
     @Test
-    void testUnbufferedRowBufferModeConfiguration() throws NoSuchFieldException, IllegalAccessException
+    void testSSTableWriterConfiguration() throws NoSuchFieldException, IllegalAccessException
     {
         CQLSSTableWriter.Builder builder = SSTableWriterImplementation.configureBuilder(writeDirectory.getAbsolutePath(),
                                                                                         CREATE_STATEMENT,
@@ -56,20 +57,6 @@ class SSTableWriterImplementationTest
 
 
         assertTrue(peekSorted(builder));
-        assertNotEquals(250, peekBufferSizeInMB(builder)); // 250 should not be set
-    }
-
-    @Test
-    void testBufferedRowBufferModeConfiguration() throws NoSuchFieldException, IllegalAccessException
-    {
-        CQLSSTableWriter.Builder builder = SSTableWriterImplementation.configureBuilder(writeDirectory.getAbsolutePath(),
-                                                                                        CREATE_STATEMENT,
-                                                                                        INSERT_STATEMENT,
-                                                                                        250,
-                                                                                        new Murmur3Partitioner());
-
-
-        assertFalse(peekSorted(builder));
         assertEquals(250, peekBufferSizeInMB(builder));
     }
 
@@ -82,20 +69,34 @@ class SSTableWriterImplementationTest
 
     static long peekBufferSizeInMB(CQLSSTableWriter.Builder builder) throws NoSuchFieldException, IllegalAccessException
     {
-        Field bufferSizeInMBField;
-        try
+        // The name of the size field has been changed in Cassandra code base.
+        // We find the field using the old name to newer one.
+        Field sizeField = findFirstField(builder.getClass(),
+                                         "bufferSizeInMB", "bufferSizeInMiB", "maxSSTableSizeInMiB");
+        sizeField.setAccessible(true);
+        return (long) sizeField.get(builder);
+    }
+
+    static Field findFirstField(Class<?> clazz, String... fieldNames) throws NoSuchFieldException, IllegalAccessException
+    {
+        Field field = null;
+        for (String fieldName : fieldNames)
         {
-            bufferSizeInMBField = ReflectionUtils.getField(builder.getClass(), "bufferSizeInMB");
-        }
-        catch (NoSuchFieldException noSuchFieldException)
-        {
-            // The bufferSizeInMB field has been renamed to bufferSizeInMiB in trunk, so we expect this to
-            // fail at some point, and we have a way to recover from the failure without causing the test
-            // to fail.
-            bufferSizeInMBField = ReflectionUtils.getField(builder.getClass(), "bufferSizeInMiB");
+            try
+            {
+                field = ReflectionUtils.getField(clazz, fieldName);
+            }
+            catch (NoSuchFieldException nsfe)
+            {
+                // ignore the exception and try with the next fieldName
+            }
         }
 
-        bufferSizeInMBField.setAccessible(true);
-        return (long) bufferSizeInMBField.get(builder);
+        if (field == null)
+        {
+            throw new NoSuchFieldException("The class does not contain any of the supplied fieldNames: " + Arrays.asList(fieldNames));
+        }
+
+        return field;
     }
 }
