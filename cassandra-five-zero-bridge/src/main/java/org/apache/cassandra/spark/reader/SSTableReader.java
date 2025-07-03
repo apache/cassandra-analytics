b2d2ab7bb7bd6cf8271ca1e6ddfdd53bf92038d6
Lukasz Antoniak <Lukasz.Antoniak@gmail.com>
2025-07-03 13:31:30 +0200

    Typo

--- cassandra-five-zero-bridge/src/main/java/org/apache/cassandra/spark/reader/SSTableReader.java
+++ cassandra-five-zero-bridge/src/main/java/org/apache/cassandra/spark/reader/SSTableReader.java
@@ -60,6 +60,8 @@ import org.apache.cassandra.db.rows.EncodingStats;
 import org.apache.cassandra.db.rows.Row;
 import org.apache.cassandra.db.rows.Unfiltered;
 import org.apache.cassandra.db.rows.UnfilteredRowIterator;
+import org.apache.cassandra.dht.Murmur3Partitioner;
+import org.apache.cassandra.dht.RandomPartitioner;
 import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.io.sstable.Descriptor;
 import org.apache.cassandra.io.sstable.ISSTableScanner;
@@ -527,7 +529,6 @@ public class SSTableReader implements SparkSSTableReader, Scannable
                                 startOffset = null;
                             }
                         }
-
                     }
                 }
             }
