f0b9ef27cce0bb24ad99654063c4dc48b01ccf36
Josh McKenzie <jmckenzie@apache.org>
2026-02-11 13:40:12 -0500

    Remove some duplication in circle config

--- scripts/get-jacoco-results.sh
+++ scripts/get-jacoco-results.sh
@@ -21,12 +21,12 @@ search_dir="."
 
 target_folder="${1}"
 
-mkdir -p $target_folder
+mkdir -p "$target_folder"
 
 find "$search_dir" -type d -path "*/build/jacoco" -print0 | while IFS= read -r -d '' dir; do
   destination_path="$target_folder/${dir:2}"
   mkdir -p "$destination_path"
-  cp -r $dir "$destination_path"
+  cp -r "$dir" "$destination_path"
   echo "Copied folder $dir to $destination_path"
 done
 
